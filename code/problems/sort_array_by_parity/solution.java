4e9f73ce81b57dbc7a4c0bb316335f32878812ea
Saumya Tiwari <taru.saumya@gmail.com>
2020-11-06 14:13:56 +0000

    Sync LeetCode submission - Sort Array By Parity (java)

---
+++ code/problems/sort_array_by_parity/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        
+        int start = 0;
+        
+        for(int i = 0; i< A.length; i++){
+            if(A[i] % 2 == 0){
+                int temp = A[i];
+                A[i] = A[start];
+                A[start++] = temp;
+            }
+        }
+        
+        return A;
+        
+    }
+}
