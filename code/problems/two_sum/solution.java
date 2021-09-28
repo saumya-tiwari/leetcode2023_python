2c9920bdd47b265e5917c09d541bf8b374dca2ef
Saumya Tiwari <taru.saumya@gmail.com>
2021-09-28 19:33:29 +0000

    Sync LeetCode submission - Two Sum (java)

--- code/problems/two_sum/solution.java
+++ code/problems/two_sum/solution.java
@@ -1,15 +1,31 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      Map<Integer,Integer> map = new HashMap<>();
         
-      for(int i = 0;i<nums.length;i++){
-          int complement = target - nums[i];
-          if(map.containsKey(complement)){
-              return new int[]{map.get(complement),i};
-          }
-          map.put(nums[i],i);
-      }
-        throw new IllegalArgumentException("No two sum solution");
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-}
+       // List<Integer> list = new ArrayList<>();
+        
+        List<Integer> list = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        
+        int[] arr = new int[2];
+        
+        
+        
+        
+        for(int i = 0; i < nums.length; i++){
+            
+            int diff = target - nums[i];
+            System.out.println(diff);
+            
+            if(list.indexOf(diff) != i && list.contains(diff)){
+                 arr[0] = i;
+                 arr[1] = list.indexOf(diff);
+                 return arr;
+            }
+            
+         
+        }
+     return arr;
+        
+    }
 }
