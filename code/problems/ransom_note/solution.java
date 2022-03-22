ef82d5d82a6e2e073fdad3f66c590d6c6d776d6c
Saumya Tiwari <taru.saumya@gmail.com>
2022-03-22 09:32:36 +0000

    Sync LeetCode submission - Ransom Note (java)

--- code/problems/ransom_note/solution.java
+++ code/problems/ransom_note/solution.java
@@ -1,31 +1,23 @@
 class Solution {
     public boolean canConstruct(String s, String t) {
-          boolean isAnagram = false;
+         
+        if(s.length() > t.length())
+            return false;
         
-        Map<Character, Integer> charMap = new HashMap<Character,Integer>();
-      
-      
+        Map<Character, Integer> magazineCounts = new HashMap<Character,Integer>();
         
-        for(int i = 0; i < t.length(); i++){
-            
-            if(!charMap.containsKey(t.charAt(i)))
-                charMap.put(t.charAt(i), 1);
-            else
-                charMap.put(t.charAt(i),charMap.get(t.charAt(i)) + 1)    ;
-          
+        for(char c: t.toCharArray()){       
+         magazineCounts.put(c, magazineCounts.getOrDefault(c, 0) + 1);    
         }
         
-        for(int i = 0; i < s.length(); i++){
-              if(charMap.containsKey(s.charAt(i))){
-                  int value = charMap.get(s.charAt(i)) - 1;
-                  
-                  if(value >=0)
-                      charMap.put(s.charAt(i),charMap.get(s.charAt(i)) - 1) ;
-                else
-                    return false;
-              }
-            else
-                return false; 
+        for(char c : s.toCharArray()){
+            int countMagazines = magazineCounts.getOrDefault(c, 0);
+            
+            if(countMagazines == 0)
+                return false;
+            
+            magazineCounts.put(c, countMagazines - 1);
+            
         }
         
         return true;   
