3e02234a702f99adb9fb19cafe132a16dba4aef8
Saumya Tiwari <taru.saumya@gmail.com>
2022-03-25 18:41:37 +0000

    Sync LeetCode submission - Symmetric Tree (java)

--- code/problems/symmetric_tree/solution.java
+++ code/problems/symmetric_tree/solution.java
@@ -16,36 +16,15 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         
-       Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        q.add(root);
-        
-        while(!q.isEmpty()){
-            TreeNode t1 = q.poll();
-            TreeNode t2 = q.poll();
-            
-            if(t1 == null && t2 == null) continue;
-            if(t1 == null || t2 == null) return false;
-            
-            if(t1.val != t2.val) return false;
-            
-            q.add(t1.left);
-            q.add(t2.right);
-            q.add(t1.right);
-            q.add(t2.left);
-        }
-        
-        return true;
-        
-        
+       return isMirror(root, root);
     }
     
- /*   public boolean isMirror(TreeNode t1, TreeNode t2){
+    private boolean isMirror(TreeNode r1, TreeNode r2){
+        if(r1 == null && r2 == null)
+          return true;
+        if(r1 == null || r2 == null)
+            return false;
         
-        if(t1 == null && t2 == null) return true;
-        if(t1 == null || t2 == null) return false;
-        
-        return (t1.val == t2.val) && isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
-        
-    } */
+        return (r1.val == r2.val) && isMirror(r1.right, r2.left) && isMirror(r1.left, r2.right);
+    }
 }
