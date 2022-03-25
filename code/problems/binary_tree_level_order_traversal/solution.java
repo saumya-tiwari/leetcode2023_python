227583e58d62880960e90d9264dafa6746a14f3b
Saumya Tiwari <taru.saumya@gmail.com>
2022-03-25 18:20:27 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- code/problems/binary_tree_level_order_traversal/solution.java
+++ code/problems/binary_tree_level_order_traversal/solution.java
@@ -4,31 +4,36 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
+    
+    List<List<Integer>> levels = new ArrayList<>();
+    
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        if(root != null)
-            q.offer(root);
-        TreeNode cur = null;
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<Integer> subList = new LinkedList<>();
-            for(int i =0; i<size;i++){
-                cur = q.poll();
-                subList.add(cur.val);
-                
-                if(cur.left != null)
-                    q.offer(cur.left);
-                
-                if(cur.right != null)
-                    q.offer(cur.right);
-            }
-            list.add(subList);
-        }
-            return list;
+        
+        if(root == null) return levels;
+        helper(root, 0);
+        return levels;
+    }
+    
+    private void helper(TreeNode node, int level){
+        
+        if(levels.size() == level)
+            levels.add(new ArrayList<>());
+        
+        levels.get(level).add(node.val);
+        
+        if(node.left != null)
+            helper(node.left, level + 1);
+        if(node.right != null)
+            helper(node.right, level + 1);
     }
 }
