b65f9050e2a9030144e2e74691f1ca3daa687adf
Saumya Tiwari <taru.saumya@gmail.com>
2022-04-18 13:14:23 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- code/problems/climbing_stairs/solution.java
+++ code/problems/climbing_stairs/solution.java
@@ -1,17 +1,14 @@
 class Solution {
     Map<Integer, Integer> cache = new HashMap<>();
     public int climbStairs(int n) {
-         if(cache.get(n) != null)
-           return cache.get(n);
         
-        int value = 0;
+    if(n <= 2)
+        return n;
         
-        if(n <= 2)
-            value = n;
-        else
-            value = climbStairs(n -1) + climbStairs(n - 2);
-        
-        cache.put(n ,  value);
-        return value;
+     if(!cache.containsKey(n)){
+         cache.put(n, climbStairs(n - 1) + climbStairs(n - 2));
+    }
+    
+    return cache.get(n);
     }
 }
