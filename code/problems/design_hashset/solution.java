f776fbc134c0535fd1726a0eee1ddbf176bd01e3
Saumya Tiwari <taru.saumya@gmail.com>
2022-04-21 17:53:52 +0000

    Sync LeetCode submission - Design HashSet (java)

--- code/problems/design_hashset/solution.java
+++ code/problems/design_hashset/solution.java
@@ -1,60 +1,60 @@
-class MyHashSet {
-    
-   List<Integer>[] set ;
-   private final int MAX_LEN =  1000000;
 
-    /** Initialize your data structure here. */
-    public MyHashSet() {
-        set = (List<Integer>[])new ArrayList[MAX_LEN];
+class MyHashSet {
+  private Bucket[] bucketArray;
+  private int keyRange;
+
+  /** Initialize your data structure here. */
+  public MyHashSet() {
+    this.keyRange = 769;
+    this.bucketArray = new Bucket[this.keyRange];
+    for (int i = 0; i < this.keyRange; ++i)
+      this.bucketArray[i] = new Bucket();
+  }
+
+  protected int _hash(int key) {
+    return (key % this.keyRange);
+  }
+
+  public void add(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].insert(key);
+  }
+
+  public void remove(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].delete(key);
+  }
+
+  /** Returns true if this set contains the specified element */
+  public boolean contains(int key) {
+    int bucketIndex = this._hash(key);
+    return this.bucketArray[bucketIndex].exists(key);
+  }
+}
+
+
+class Bucket {
+  private LinkedList<Integer> container;
+
+  public Bucket() {
+    container = new LinkedList<Integer>();
+  }
+
+  public void insert(Integer key) {
+    int index = this.container.indexOf(key);
+    if (index == -1) {
+      this.container.addFirst(key);
     }
-    
-    public int getIndex(int key){
-        return key % MAX_LEN;
-    }
-    
-    public int getPos(int key, int index){
-        List<Integer> temp = set[index];
-        if (temp == null) {
-            return -1;
-        }
-        // Iterate all the elements in the bucket to find the target key.
-        for (int i = 0; i < temp.size(); ++i) {
-            if (temp.get(i) == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
-    public void add(int key) {
-       int index = getIndex(key);
-       int pos = getPos(key, index);
-       
-        if (pos < 0) {
-            // Add new key if key does not exist.
-            if (set[index] == null) {
-                set[index] = new ArrayList<Integer>();
-            }
-            set[index].add(key);
-        }
-           
-    }
-    
-    public void remove(int key) {
-       int index = getIndex(key);
-       int pos = getPos(key, index);
-        if (pos >= 0) {
-            // Remove the key if key exists.
-            set[index].remove(pos);
-        }
-    }
-    
-    /** Returns true if this set did not already contain the specified element */
-    public boolean contains(int key) {
-       int index = getIndex(key);
-       int pos = getPos(key, index);
-        return pos >= 0;
-    }
+  }
+
+  public void delete(Integer key) {
+    this.container.remove(key);
+  }
+
+  public boolean exists(Integer key) {
+    int index = this.container.indexOf(key);
+    return (index != -1);
+  }
 }
 
 /**
