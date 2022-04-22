6fefaf91cb6494326134df520fc6162eabe65fd8
Saumya Tiwari <taru.saumya@gmail.com>
2022-04-22 19:21:57 +0000

    Sync LeetCode submission - Design HashMap (java)

--- code/problems/design_hashmap/solution.java
+++ code/problems/design_hashmap/solution.java
@@ -1,70 +1,91 @@
-import javafx.util.Pair;
-
 class MyHashMap {
-    private final int MAX_LEN = 100000;             // the amount of buckets
-    private List<Pair<Integer, Integer>>[] map;     // hash map implemented by array
     
-    /** Returns the corresponding bucket index. */
-    private int getIndex(int key) {
-        return key % MAX_LEN;
+    private int key_space;
+    private List<Bucket> hash_table;
+
+    public MyHashMap() {
+        
+        this.key_space = 2069;
+        this.hash_table = new ArrayList<>();
+        for (int i = 0; i < this.key_space; ++i) {
+      this.hash_table.add(new Bucket());
+    }
+        
     }
     
-    /** Search the key in a specific bucket. Returns -1 if the key does not existed. */
-    private int getPos(int key, int index) {
-        // Each bucket contains a list.
-        List<Pair<Integer, Integer>> temp = map[index];
-        if (temp == null) {
-            return -1;
+    public void put(int key, int value) {
+        int hash_key = key % this.key_space;
+        this.hash_table.get(hash_key).update(key, value);
+    }
+    
+    public int get(int key) {
+        
+          int hash_key = key % this.key_space;
+        return this.hash_table.get(hash_key).get(key);
+        
+    }
+    
+    public void remove(int key) {
+          int hash_key = key % this.key_space;
+    this.hash_table.get(hash_key).remove(key);
+    }
+}
+
+ class Pair<U,V>{
+     
+     public U first;
+     public V second;
+     
+     public Pair(U first, V second){
+         this.first = first;
+         this.second = second;
+     }
+        
+    }
+
+class Bucket{
+    
+    private List<Pair<Integer, Integer>> bucket;
+    
+    public Bucket(){
+        this.bucket = new ArrayList<>();
+    }
+    
+    public Integer get(Integer key){
+        for(Pair<Integer, Integer> pair: this.bucket){
+            if(pair.first.equals(key))
+                return pair.second;
         }
-        // Iterate all the elements in the bucket to find the target key.
-        for (int i = 0; i < temp.size(); ++i) {
-            if (temp.get(i).getKey() == key) {
-                return i;
-            }
-        }
+        
         return -1;
     }
-
-    /** Initialize your data structure here. */
-    public MyHashMap() {
-        map = (List<Pair<Integer, Integer>>[])new ArrayList[MAX_LEN];
-    }
     
-    /** value will always be positive. */
-    public void put(int key, int value) {
-        int index = getIndex(key);
-        int pos = getPos(key, index);
-        if (pos < 0) {
-            // Add new (key, value) pair if key is not existed.
-            if (map[index] == null) {
-                map[index] = new ArrayList<Pair<Integer, Integer>>();
+    public void remove(Integer key){
+          for(Pair<Integer, Integer> pair: this.bucket){
+            if(pair.first.equals(key)){
+                 this.bucket.remove(pair);
+                break;
             }
-            map[index].add(new Pair(key, value));
-        } else {
-            // Update the value if key is existed.
-            map[index].set(pos, new Pair(key, value));
+               
         }
     }
     
-    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
-    public int get(int key) {
-        int index = getIndex(key);
-        int pos = getPos(key, index);
-        if (pos < 0) {
-            return -1;
-        } else {
-            return map[index].get(pos).getValue();
+    public void update(Integer key, Integer value){
+        boolean found = false;
+         for(Pair<Integer, Integer> pair: this.bucket){
+            if(pair.first.equals(key)){
+                pair.second = value;
+                found = true;
+            }
+               
+        }
+        
+        if(!found){
+            this.bucket.add(new Pair<Integer, Integer>(key, value));
         }
     }
     
-    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
-    public void remove(int key) {
-        int index = getIndex(key);
-        int pos = getPos(key, index);
-        if (pos >= 0) {
-            map[index].remove(pos);
-        }
-    }
+   
 }
 
 /**
