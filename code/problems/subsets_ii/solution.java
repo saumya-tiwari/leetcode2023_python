378c0efc0965e2257b4a077e19c70eeda05e9932
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-10 05:53:25 +0000

    Sync LeetCode submission - Subsets II (java)

--- code/problems/subsets_ii/solution.java
+++ code/problems/subsets_ii/solution.java
@@ -1,43 +1,30 @@
 class Solution {
-    
-    List<List<Integer>> subsets = new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
     
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         
-        List<Integer> op = new ArrayList<>();
-        Map<List<Integer>, Integer> map = new HashMap<>();
         Arrays.sort(nums);
         
-        return findSubsets(nums, op, map);
+        Set<List<Integer>> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        backtrack(0, list, nums, set);
+        return new ArrayList<>(set);      
         
     }
     
-    public List<List<Integer>> findSubsets(int[] nums, List<Integer> op, Map<List<Integer>, Integer> nMap){
+    public void backtrack(int index, List<Integer> list, int[] nums, Set<List<Integer>> set){
         
-        if(nums.length == 0){
-            if(!nMap.containsKey(op)){
-               subsets.add(op);
-               nMap.put(op, 1);
-            }
-            else{
-                nMap.put(op, nMap.get(op) + 1);
-            }
-            return subsets;
+        if(index >= nums.length){
+            set.add(new ArrayList<>(list));
+            return;
         }
         
-         List<Integer> op1 = new ArrayList<>(op);
-         List<Integer> op2 = new ArrayList<>(op);
+        list.add(nums[index]);
+        Collections.sort(list);
+        backtrack(index + 1, list, nums, set);
         
-        op2.add(nums[0]);
-        
-        int[] tmp = Arrays.copyOfRange(nums, 1, nums.length);
-        
-        findSubsets(tmp, op1, nMap);
-        findSubsets(tmp, op2, nMap);
-        
-        return subsets;
-        
-        
-        
+        list.remove(list.size() - 1);
+        Collections.sort(list);
+        backtrack(index + 1, list, nums, set);
     }
 }
