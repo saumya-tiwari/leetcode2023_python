44cf8ec363da51696d6b953352ce977c254cc327
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-12 17:00:22 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- code/problems/contains_duplicate/solution.java
+++ code/problems/contains_duplicate/solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
-        Arrays.sort(nums);
-        int i =0;
-        int j =1;
+       Arrays.sort(nums);
         
-      while(i < nums.length && j < nums.length){
-          if(nums[i] != nums[j]){
-              i++;
-              j++;
-          }else return true;
-              
-      }
+        int i  = 0;
+        int j  = 1;
+        
+        while(i < nums.length && j < nums.length ) {
+            
+            if(nums[i] == nums[j]){
+               return true;
+            }
+             i++;
+             j++;
+        }
         
         return false;
         
