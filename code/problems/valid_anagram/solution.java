7d801760dc17822d8d03a75686c7f1905ff13234
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-12 18:03:21 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- code/problems/valid_anagram/solution.java
+++ code/problems/valid_anagram/solution.java
@@ -1,31 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        boolean isAnagram = false;
+        int len1 = s.length();
+        int len2 = t.length();
         
-        Map<Character, Integer> anagramMap1 = new HashMap<Character,Integer>();
-        Map<Character, Integer> anagramMap2 = new HashMap<Character,Integer>();
+        if(len1 != len2)
+            return false;
         
-      
+        Map<Character, Integer> countMap = new HashMap<>();
         
-        for(int i = 0; i < s.length(); i++){
-            
-            if(!anagramMap1.containsKey(s.charAt(i)))
-                anagramMap1.put(s.charAt(i), 1);
-            else
-                anagramMap1.put(s.charAt(i),anagramMap1.get(s.charAt(i)) + 1)    ;
-          
+        for(int i  = 0; i < len1; i++){
+            countMap.put(s.charAt(i), countMap.getOrDefault(s.charAt(i), 0) + 1);
+            countMap.put(t.charAt(i), countMap.getOrDefault(t.charAt(i), 0) - 1);
         }
         
-        for(int i = 0; i < t.length(); i++){
-              if(!anagramMap2.containsKey(t.charAt(i)))
-                anagramMap2.put(t.charAt(i), 1);
-            else
-                anagramMap2.put(t.charAt(i),anagramMap2.get(t.charAt(i)) + 1)   ; 
+        for(int value: countMap.values()){
+            if(value != 0 )
+                return false;
         }
         
-        
-        return anagramMap2.equals(anagramMap1);
+        return true;
         
     }
 }
