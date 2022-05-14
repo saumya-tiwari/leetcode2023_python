6ef08b90d26c7e831e5cd44e2add47a4cfcae80b
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-14 19:39:27 +0000

    Sync LeetCode submission - Maximum Depth of Binary Tree (java)

--- code/problems/maximum_depth_of_binary_tree/solution.java
+++ code/problems/maximum_depth_of_binary_tree/solution.java
@@ -15,13 +15,42 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null)
-            return 0;
-        else{
-            int left_height = maxDepth(root.left);
-            int right_height = maxDepth(root.right);
-            return Math.max(left_height, right_height) + 1;
+        
+       // if(root == null)
+          //  return 0;
+        
+      // int lDepth = maxDepth(root.left);
+      // int rDepth = maxDepth(root.right);
+        
+        //adding one because that will be depth of the single node tree and then adding that to max of left
+        //and right subtree
+      // return Math.max(lDepth, rDepth) + 1;
+      
+         if (root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        
+        int level = 0;
+        
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            
+            for(int i  = 0; i <size; i++){
+                TreeNode node = q.poll();
+                
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+                
+            }
+            
+            level += 1;
+            
+            
+            
         }
         
+        return level;
     }
 }
