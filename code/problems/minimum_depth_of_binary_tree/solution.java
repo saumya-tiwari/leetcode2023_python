037d8faca562822f587ba101ab6903ca74af0e02
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-14 19:46:28 +0000

    Sync LeetCode submission - Minimum Depth of Binary Tree (java)

--- code/problems/minimum_depth_of_binary_tree/solution.java
+++ code/problems/minimum_depth_of_binary_tree/solution.java
@@ -14,15 +14,37 @@
  * }
  */
 class Solution {
+
     public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         
-        if(root == null)
-            return 0;
+        int level = 1;
         
-        int left = minDepth(root.left);
-        int right = minDepth(root.right);
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            
+            for(int i  = 0; i <size; i++){
+                TreeNode node = q.poll();
+                if (isLeafNode(node))   return level;
+                if(node.left != null) q.add(node.left);
+                if(node.right != null) q.add(node.right);
+                
+            }
+            
+            level += 1;
+            
+            
+            
+        }
         
-        return (left == 0 || right == 0) ? left + right + 1: Math.min(left,right) + 1;
-        
+        return level;
+    }
+    
+    private boolean isLeafNode(TreeNode curr){
+        return curr != null && curr.left == null && curr.right == null;
     }
 }
