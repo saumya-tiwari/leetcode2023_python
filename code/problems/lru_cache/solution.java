39f3857e010e40b10acd2978a923dc1762a7cc11
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-16 09:40:06 +0000

    Sync LeetCode submission - LRU Cache (java)

--- code/problems/lru_cache/solution.java
+++ code/problems/lru_cache/solution.java
@@ -1,25 +1,101 @@
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class LRUCache {
     
-    private int capacity;
+    class DLinkedNode{
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+    }
+    
+    private void addNewNode(DLinkedNode node){
+        
+        //add just after head
+        
+        node.prev = head;
+        node.next = head.next;
+        
+        head.next.prev = node;
+        head.next = node;
+    }
+    
+    private void removeNode(DLinkedNode node){
+        //remove existing node form list
+        
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+        
+        prev.next = next;
+        next.prev = prev;
+    }
+    
+    private void moveToHead(DLinkedNode node){
+        
+        removeNode(node);
+        addNewNode(node);
+        
+    }
+
+    private DLinkedNode popTail(){
+        
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+        
+    }
+    
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    int capacity;
+    int size;
+    private DLinkedNode head, tail;
+
 
     public LRUCache(int capacity) {
-        super(capacity, 0.75F, true);
+        this.size = 0;
         this.capacity = capacity;
+        
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        return super.getOrDefault(key, -1);
+        
+        DLinkedNode node = cache.get(key);
+        
+        if(node != null){
+            moveToHead(node);
+            return node.value;
+        }else
+            return -1;
         
     }
     
     public void put(int key, int value) {
-        super.put(key, value);
         
-    }
-    
-     @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity; 
+        DLinkedNode node1 = cache.get(key);
+        
+        if(node1 == null){
+            DLinkedNode node = new DLinkedNode();
+            node.value = value;
+            node.key = key;
+            
+            cache.put(key, node);
+            addNewNode(node);
+            
+            ++size;
+            
+            if(size > capacity){
+                DLinkedNode node2 = popTail();
+                cache.remove(node2.key);
+                size--;
+            }
+        }else{
+            node1.value = value;
+            moveToHead(node1);
+        }
+        
     }
 }
 
