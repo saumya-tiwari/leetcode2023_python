e2d7070c84be608a7147e3601737bbac05920ca8
Saumya Tiwari <taru.saumya@gmail.com>
2022-05-27 11:34:52 +0000

    Sync LeetCode submission - Top K Frequent Elements (java)

--- code/problems/top_k_frequent_elements/solution.java
+++ code/problems/top_k_frequent_elements/solution.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       
-        Map<Integer, Integer> count = new HashMap<>();
         
-        for(int n: nums){         
-           count.put(n, count.getOrDefault(n, 0) + 1);           
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        
+        for(int n: nums)
+            freqMap.put(n, freqMap.getOrDefault(n, 0) + 1);
+        
+        
+        Queue<Integer> q = new PriorityQueue<>((c1, c2) -> freqMap.get(c1) - freqMap.get(c2));
+        
+        
+        for(int n : freqMap.keySet()){
+            q.add(n);
+            
+            if(q.size() > k) q.poll();
         }
         
-         Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+        int[] res = new int[k];
         
-        for(int n: count.keySet()){
-        heap.add(n);
-           if(heap.size() > k)
-            heap.poll();
-        }
-         
-    
+        for(int i = k - 1; i >=0; i--)
+            res[i] = q.poll();
         
-        int arr[] = new int[k];
-      
-        for(int i = k - 1; i >= 0 ; i--)
-            arr[i] = heap.poll();
-        
-        return arr;
-                
-        
+        return res;
     }
 }
