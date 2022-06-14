77a838c2b351bc7b467d4170b878dda995dc5637
Saumya Tiwari <taru.saumya@gmail.com>
2022-06-14 19:33:02 +0000

    Sync LeetCode submission - Group Anagrams (java)

--- code/problems/group_anagrams/solution.java
+++ code/problems/group_anagrams/solution.java
@@ -1,20 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
-       Map<String, List<String>> map = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         
-        for(String anagrams: strs){
-            char[] ch = anagrams.toCharArray();
+        int[] count = new int[26];
+        
+        for(String str: strs){
             
-            Arrays.sort(ch);
+            char[] ch = str.toCharArray();
+            Arrays.fill(count, 0);
+            for(char c: ch) count[c - 'a']++;
             
-            String s = String.valueOf(ch);
+            StringBuilder sb = new StringBuilder("");
             
-            map.putIfAbsent(s, new ArrayList<String>());
-            map.get(s).add(anagrams);
+            for(int i  = 0; i < 26;i++){
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            
+            map.putIfAbsent(sb.toString(), new ArrayList<>());
+            map.get(sb.toString()).add(str);
         }
         
-        return new ArrayList<>(map.values());
-    }
+        return new ArrayList(map.values());
         
     }
+}
