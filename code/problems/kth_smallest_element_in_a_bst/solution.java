2d7ea1e759dd044ddd3f0b754e4c0503f7945f03
Saumya Tiwari <taru.saumya@gmail.com>
2022-06-25 07:54:41 +0000

    Sync LeetCode submission - Kth Smallest Element in a BST (java)

--- code/problems/kth_smallest_element_in_a_bst/solution.java
+++ code/problems/kth_smallest_element_in_a_bst/solution.java
@@ -14,24 +14,18 @@
  * }
  */
 class Solution {
+
     public int kthSmallest(TreeNode root, int k) {
-        
-        List<Integer> list = new ArrayList<>();
-        
-        populateHeap(list, root);
-        
+       List<Integer> list = new ArrayList<>();
+        inorder(root, list);
         return list.get(k - 1);
-        
-        
-        
     }
-    
-     public void populateHeap(List<Integer> heap, TreeNode root){
-        if(root== null) return;
-        
-        populateHeap(heap, root.left);
-        heap.add(root.val);
-        populateHeap(heap, root.right);
-        
+
+    List<Integer> inorder(TreeNode root, List<Integer> list) {
+        if (root == null) return list;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+        return list;
     }
 }
